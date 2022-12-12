30a43568713c1a38fd6c8e07d076f2b37f85a326
Tetri Mesquita <tetri.mesquita@gmail.com>
2022-12-12 13:54:42 -0300

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SimpleEmailApp.Models;
+using SimpleEmailApp.Services.EmailService;
+
+namespace SimpleEmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            //var subject = request.Subject;
+            //for (int i = 0; i < 20; i++)
+            //{
+            //request.Subject = $"#{i} {subject}";
+            //Thread.Sleep(200);
+            _emailService.SendEmail(request);
+            //}
+            return Ok();
+        }
+    }
+}
---
+++ Services/EmailService/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+
+using MimeKit;
+using MimeKit.Text;
+
+using SimpleEmailApp.Models;
+
+namespace SimpleEmailApp.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("Email:Username").Value));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_config.GetSection("Email:Host").Value, Convert.ToInt32(_config.GetSection("Email:Port").Value), SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config.GetSection("Email:Username").Value, _config.GetSection("Email:Password").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
